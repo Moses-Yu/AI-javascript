1ce1840e89763ea3fe2e84cc895259db7fc7933d
Moses-Yu <mosesyumss@gmail.com>
2021-06-01 00:41:28 +0900

    2021-05-31

--- AI.js
+++ AI.js
@@ -1,35 +1,82 @@
-let v, i, iy, lastF, adjust = 0;
+let v, i, iy, lastF, lastY, adjust = 0;
 let jumpforce = 6;
 let downforce = 0.5;
-let gravity = 1.1;
+let gravity = 1.09;
 let xMove = 3;
 let xScreen = xMove;
 let tries = 0;
 let hit = false;
-
+let hitOnBottom = false;
 let AI = [];
 
-
+let col1, col2, col3;
+let col = [];
 function setup() {
-  createCanvas(1200, 800);
-  v = createVector(100, 0);
-  
-
+  let cvs = createCanvas(1200, 800).center('horizontal');
+  cvs.mousePressed(pause);
+  frameRate(100);
+  v = createVector(100, 400);
   i = 100;
+  col1 = random(0,255);
+  col2 = random(0,255);
+  col3 = random(0,255);
+
+  col[0] = {r:0,g:0,b:0};
+  character();
 }
 
 function draw() {
-  background(220);
+  background(0);
 
-  translate(0 - xScreen, 400);
+  translate(0 - xScreen, 0);
 
+  ending();
   character();
   obstacles();
-  
-  text("number of tries: "+ tries, 0+xScreen, 0);
-  text("position: "+ i, 0+xScreen, 20);
-  text("last fail position: "+ lastF, 0+xScreen, 40);
+  fill(255);
+  textSize(12);
+  text("number of tries: "+ tries, 0+xScreen, 400);
+  text("position: "+ i + " " + floor(v.y), 0+xScreen, 420);
+  text("last fail position: "+ lastF + " " + lastY, 0+xScreen, 440);
+  text("adjust: "+ adjust, 0+xScreen, 460);
+
+   if(paused == true){
+    fill(255);
+    textSize(100);
+    text("Paused",300+xScreen,400);
+    noLoop();
+  }
+
+  for(iy = 100;iy<3000;iy+=3){
+    if(AI[iy] == true){
+      fill(0, 128, 0);
+      rect(iy,780,3,3);
+    }else{
+      fill(255);
+      rect(iy,780,3,3);
+    }
+  }
+
   i+=xMove;
+}
+
+let paused = false;
+function pause(){
+  if(paused == false){
+    paused = true;
+  }else{
+    paused = false;
+    loop();
+  } 
+}
+
+function ending(){
+  if(i>2700){
+    fill(255);
+    textSize(100);
+    text("END",3000,400);
+    noLoop();
+  }
 }
 
 function jump() {
@@ -38,6 +85,10 @@ function jump() {
 }
 
 function character() {
+  col[0].r = random(0,255);
+  col[0].g = random(0,255);
+  col[0].b = random(0,255);
+  fill(col[0].r,col[0].g,col[0].b);
   rect(v.x, v.y, 50, 50);
   if(AI[i] == true)
     jump();
@@ -50,63 +101,89 @@ function character() {
     v.set(v.x + xMove, v.y + downforce);
   }
 
-  if (v.y > 400) {
+  if (floor(v.y) > 610) {
+    hitOnBottom = true;
+    AIprogram();
+  }
+  if (floor(v.y) > 800) {
     resetPos();
+
   }
   xScreen += xMove;
- 
+
 }
 
-function obstacles() {
-  rect(300, 100, 200, 300);
-  rect(300, -400, 200, 300);
-  hit = collideRectRect(v.x,v.y,50,50,300,100,200,300);  
-  if(hit)
-    resetPos();
-  hit = collideRectRect(v.x,v.y,50,50,300,-400,200,300);
-   if(hit)
-    resetPos();
-  
-  rect(800, 0, 200, 400);
-  rect(800, -400, 200, 200);
-  hit = collideRectRect(v.x,v.y,50,50,800, 0, 200, 400);
-  if(hit)
-    resetPos();
-  hit = collideRectRect(v.x,v.y,50,50,800, -400, 200, 200);
-  if(hit)
-    resetPos();
-  
-  rect(1300, 200, 200, 200);
-  rect(1300, -400, 200, 400);
-  hit = collideRectRect(v.x,v.y,50,50,1300, 200, 200, 200);
-  if(hit)
-    resetPos();
-  hit = collideRectRect(v.x,v.y,50,50,1300, -400, 200, 400);
-  if(hit)
-    resetPos();
-  
-  rect(300, 100, 200, 300);
-}
+
 
 function resetPos(){
+  lastY = floor(v.y);
   xScreen = 0;
-  v.set(100, 0);
+  v.set(100, 400);
   jump();
   tries++;
   AIprogram();
+  col1 = random(0,255);
+  col2 = random(0,255);
+  col3 = random(0,255);
 }
 
+
+var lastJump = 0;
 function AIprogram(){
-  if(lastF == i){
-    AI[i-3] = false;
-    AI[i-(30*(adjust-2))] = false;
-    AI[i-30*adjust] = true;
+
+  if(hitOnBottom == true){
+    AI[i - 6] = true;
+    hitOnBottom = false;
+  }else if(lastF == i){
+
+    AI[lastJump] = false;
+    AI[i - 6*adjust - 12] = true;
+    lastJump = i - 6*adjust - 12;
     adjust++;
+    
   }else{
-    AI[i-3] = true;
-    lastF = i;
-    adjust = 1;
+    AI[i - 6] = true;
+    adjust = 0;
+    lastJump = 0;
   }
-  
+
+  lastF = i;
   i = 100;
+}
+
+
+function obstacles() {  
+
+  makeObs(0, 300);
+  makeObs(1, 450);
+  makeObs(2, 400);
+  makeObs(3, 500);
+  makeObs(4, 450);
+  makeObs(5, 400);
+  makeObs(6, 350);
+  
+}
+
+var obsWidth = 150;
+var distObs = 380;
+var btwObs = 220;
+var totalHeight = 800;
+var startObs = 300;
+
+
+function makeObs(num, between){
+
+
+  fill(col1,col2,col3);
+  noStroke();
+
+  rect(startObs + num*distObs, 0, obsWidth, between-btwObs/2);
+  rect(startObs + num*distObs, between+btwObs/2, obsWidth, totalHeight-between-btwObs/2);
+
+  hit = collideRectRect(v.x,v.y,50,50, startObs + num*distObs, 0, obsWidth, between-btwObs/2);  
+  if(hit)
+    resetPos();
+  hit = collideRectRect(v.x,v.y,50,50, startObs + num*distObs, between+btwObs/2, obsWidth, totalHeight-between-btwObs/2);
+  if(hit)
+    resetPos();
 }
--- Human.js
+++ Human.js
@@ -1,7 +1,7 @@
-let v, i, iy, lastF, adjust = 0;
+let v, i, iy, lastF, lastY;
 let jumpforce = 6;
 let downforce = 0.5;
-let gravity = 1.1;
+let gravity = 1.09;
 let xMove = 3;
 let xScreen = xMove;
 let tries = 0;
@@ -9,37 +9,59 @@ let hit = false;
 
 
 
-
+let col1, col2, col3;
+let col = [];
 function setup() {
-  createCanvas(1200, 800);
-  v = createVector(100, 0);
-  
-
+  createCanvas(1200, 800).center('horizontal');
+  frameRate(100);
+  v = createVector(100, 400);
   i = 100;
+  col1 = random(0,255);
+  col2 = random(0,255);
+  col3 = random(0,255);
+
+  col[0] = {r:0,g:0,b:0};
+  character();
 }
 
 function draw() {
-  background(220);
+  background(0);
+  translate(0 - xScreen, 0);
 
-  translate(0 - xScreen, 400);
-
+  ending();
   character();
   obstacles();
-  
-  text("number of tries: "+ tries, 0+xScreen, 0);
-  text("position: "+ i, 0+xScreen, 20);
-  text("last fail position: "+ lastF, 0+xScreen, 40);
+
+  fill(255);
+  textSize(12);
+  text("number of tries: "+ tries, 0+xScreen, 400);
+  text("position: "+ i + " " + floor(v.y), 0+xScreen, 420);
+  text("last fail position: "+ lastF + " " + lastY, 0+xScreen, 440);
+
   i+=xMove;
 }
 
-function mousePressed() {
+function ending(){
+  if(i>2700){
+    fill(255);
+    textSize(100);
+    text("END",3000,400);
+    noLoop();
+  }
+}
+
+function jump() {
   jumpforce = 5;
   downforce = 0.5;
 }
 
 function character() {
+  col[0].r = random(0,255);
+  col[0].g = random(0,255);
+  col[0].b = random(0,255);
+  fill(col[0].r,col[0].g,col[0].b);
   rect(v.x, v.y, 50, 50);
-  
+ 
   if (jumpforce > 0.5) {
     v.set(v.x + xMove, v.y - jumpforce);
     jumpforce /= gravity;
@@ -48,50 +70,63 @@ function character() {
     v.set(v.x + xMove, v.y + downforce);
   }
 
-  if (v.y > 400) {
+  if (floor(v.y) > 800) {
     resetPos();
   }
+
   xScreen += xMove;
- 
+
 }
 
-function obstacles() {
-  rect(300, 100, 200, 300);
-  rect(300, -400, 200, 300);
-  hit = collideRectRect(v.x,v.y,50,50,300,100,200,300);  
-  if(hit)
-    resetPos();
-  hit = collideRectRect(v.x,v.y,50,50,300,-400,200,300);
-   if(hit)
-    resetPos();
-  
-  rect(800, 0, 200, 400);
-  rect(800, -400, 200, 200);
-  hit = collideRectRect(v.x,v.y,50,50,800, 0, 200, 400);
-  if(hit)
-    resetPos();
-  hit = collideRectRect(v.x,v.y,50,50,800, -400, 200, 200);
-  if(hit)
-    resetPos();
-  
-  rect(1300, 200, 200, 200);
-  rect(1300, -400, 200, 400);
-  hit = collideRectRect(v.x,v.y,50,50,1300, 200, 200, 200);
-  if(hit)
-    resetPos();
-  hit = collideRectRect(v.x,v.y,50,50,1300, -400, 200, 400);
-  if(hit)
-    resetPos();
-  
-  rect(300, 100, 200, 300);
+function mousePressed() {
+  jump();
 }
 
 function resetPos(){
+  lastY = floor(v.y);
   xScreen = 0;
-  v.set(100, 0);
-  mousePressed();
+  v.set(100, 400);
+  jump();
   tries++;
-  lastF = i;
   i = 100;
+  col1 = random(0,255);
+  col2 = random(0,255);
+  col3 = random(0,255);
 }
 
+
+function obstacles() {  
+
+  makeObs(0, 300);
+  makeObs(1, 450);
+  makeObs(2, 400);
+  makeObs(3, 500);
+  makeObs(4, 450);
+  makeObs(5, 400);
+  makeObs(6, 350);
+  
+}
+
+var obsWidth = 150;
+var distObs = 380;
+var btwObs = 220;
+var totalHeight = 800;
+var startObs = 300;
+
+
+function makeObs(num, between){
+
+
+  fill(col1,col2,col3);
+  noStroke();
+
+  rect(startObs + num*distObs, 0, obsWidth, between-btwObs/2);
+  rect(startObs + num*distObs, between+btwObs/2, obsWidth, totalHeight-between-btwObs/2);
+
+  hit = collideRectRect(v.x,v.y,50,50, startObs + num*distObs, 0, obsWidth, between-btwObs/2);  
+  if(hit)
+    resetPos();
+  hit = collideRectRect(v.x,v.y,50,50, startObs + num*distObs, between+btwObs/2, obsWidth, totalHeight-between-btwObs/2);
+  if(hit)
+    resetPos();
+}
